0f1e20aadfc9aa93b19c4b21c39c3b795bf58cdb
Charles Busatta Cunico <tcharlye@gmail.com>
2019-10-13 03:56:20 +0200

    Add Admin for Boat. Remove BoatController and unused files.

---
+++ src/Admin/BoatAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+final class BoatAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', TextType::class);
+        $formMapper->add('length', TextType::class);
+        $formMapper->add('year', IntegerType::class);
+        $formMapper->add('color', ColorType::class);
+
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+        $datagridMapper->add('length');
+        $datagridMapper->add('year');
+        $datagridMapper->add('color');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+        $listMapper->add('length');
+        $listMapper->add('year');
+//        $listMapper->add('color');
+        $listMapper->add('color', 'string', ['template' => 'list_color.html.twig']);
+
+    }
+}
